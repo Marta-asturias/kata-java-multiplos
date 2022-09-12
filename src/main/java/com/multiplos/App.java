2ed25b2d284e19cc973a5c370499e467bfe1f730
Marta-asturias <marta.gonzalez@domoticadavinci.com>
2022-09-12 13:00:31 +0200

    first commit

---
+++ src/main/java/com/multiplos/App.java
@@ -0,0 +1,22 @@
+package com.multiplos;
+
+/**
+ * Hello world!
+ */
+public final class App {
+    private App() {
+    }
+
+    /**
+     * Says hello to the world.
+     * @param args The arguments of the program.
+     */
+    public static void main(String[] args) {
+        int num = 5;
+        for(int i = 1; i <= 10; ++i)
+        {
+            System.out.printf("%d * %d = %d \n", num, i, num * i);
+        }
+    }
+    }
+
